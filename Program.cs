a4deff0b0398e7843f812936d476586e916bcba4
kamilmatyja <matyja_kamil@o2.pl>
2023-04-11 21:21:52 +0200

    Release ver. 1.0.0

--- Program.cs
+++ Program.cs
@@ -92,6 +92,68 @@ public class Program
             Console.WriteLine();
         }
 
-        String x = Console.ReadLine();
+        //daneDoArkusza(random);
+    }
+
+    static void daneDoArkusza(Random random)
+    {
+        double[,,] arr = new double[20, 20, 7];
+
+        for (int i = 0; i < 20; i++)
+        {
+            BST treeBST = new BST();
+            AVL treeAVL = new AVL();
+
+            for (int j = 0; j <= i; j++)
+            {
+                int randomKey;
+                int randomValue;
+
+                randomKey = random.Next(1, 100);
+                randomValue = random.Next(1, 100);
+
+                DateTime start1 = DateTime.Now;
+                treeBST.Insert(randomKey, randomValue);
+                DateTime end1 = DateTime.Now;
+                TimeSpan ts1 = (end1 - start1);
+
+                DateTime start2 = DateTime.Now;
+                treeAVL.Insert(randomKey, randomValue);
+                DateTime end2 = DateTime.Now;
+                TimeSpan ts2 = (end2 - start2);
+
+                arr[i, j, 0] = j + 1;
+                arr[i, j, 1] = randomKey;
+                arr[i, j, 2] = randomValue;
+                arr[i, j, 3] = ts1.TotalMilliseconds;
+                arr[i, j, 4] = ts2.TotalMilliseconds;
+            }
+
+            for (int j = 0; j <= i; j++)
+            {
+                int randomKey = (int)arr[i, j, 1];
+
+                DateTime start3 = DateTime.Now;
+                NodeBST nodeBST = treeBST.Search(randomKey, treeBST.root);
+                DateTime end3 = DateTime.Now;
+                TimeSpan ts3 = (end3 - start3);
+
+                DateTime start4 = DateTime.Now;
+                NodeAVL nodeAVL = treeAVL.Search(randomKey, treeAVL.root);
+                DateTime end4 = DateTime.Now;
+                TimeSpan ts4 = (end4 - start4);
+
+                arr[i, j, 5] = ts3.TotalMilliseconds;
+                arr[i, j, 6] = ts4.TotalMilliseconds;
+            }
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.WriteLine("{0};{1};{2};{3};{4};{5};{6};", arr[i, j, 0], arr[i, j, 1], arr[i, j, 2], arr[i, j, 3], arr[i, j, 4], arr[i, j, 5], arr[i, j, 6]);
+            }
+        }
     }
 }
